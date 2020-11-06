15480171b53fb9277797853f287f2349513b6199
youngseok <dblys152@gmail.com>
2020-11-07 00:05:56 +0900

    ..

--- routes/blog/postMng.js
+++ routes/blog/postMng.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const dbConfig = require('./../../config/database.js');
 const comCd = require('./../common/comCd.js');
 
 router.get('/:mbrNo/:postNo', function(req, res) {
@@ -7,12 +8,10 @@ router.get('/:mbrNo/:postNo', function(req, res) {
   let postNo = req.params.postNo;
   let comCdList = null;
   comCd.comCdList(function(err, result) {
-    console.log(result[0].COM_CD_NM);
+    console.log(result);
     comCdList = result[0].COM_CD_NM;
+    res.send('This is postMng! ' + mbrNo + '/' + postNo + ' : ' + comCdList);
   });
-  console.log('test1');
-  console.log(comCdList);
-  res.send('This is postMng! ' + mbrNo + '/' + postNo + ' : ' + comCdList);
 });
 
 module.exports = router;
